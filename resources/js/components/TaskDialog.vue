15f4f09bb9e69bc835209aa6f4082e0f80a3b691
Leonardo Gil <leogilc@gmail.com>
2025-06-13 14:09:04 -0300

    initial of create task dialog

---
+++ resources/js/components/TaskDialog.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog'
+import { type PropType, defineProps } from 'vue'
+
+const props = defineProps({
+    triggerText: { type: String, default: 'Add Task' },
+    triggerClass: { type: String, default: 'absolute right-4 top-4' },
+    dialogTitle: { type: String, default: 'Add Task' },
+    dialogDescription: { type: String, default: 'Just an example.' },
+})
+</script>
+
+<template>
+    <Dialog>
+        <DialogTrigger as-child>
+            <Button :class="triggerClass">
+                {{ triggerText }}
+            </Button>
+        </DialogTrigger>
+        <DialogContent class="sm:max-w-[425px]">
+            <DialogHeader>
+                <DialogTitle>{{ dialogTitle }}</DialogTitle>
+                <DialogDescription>
+                    {{ dialogDescription }}
+                </DialogDescription>
+            </DialogHeader>
+
+            <p>Conteúdo do form aqui</p>
+
+            <DialogFooter>
+                <Button type="submit" form="dialogForm">
+                    Save changes
+                </Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
+</template>
--- resources/js/pages/Tasks/Index.vue
+++ resources/js/pages/Tasks/Index.vue
@@ -2,6 +2,8 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
+import PlaceholderPattern from '../../components/PlaceholderPattern.vue';
+import TaskDialog from '@/components/TaskDialog.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -14,17 +16,24 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 <template>
     <AppLayout :breadcrumbs="breadcrumbs">
+
         <Head title="Tasks" />
 
+        <TaskDialog triggerText="Add Task" triggerClass="absolute right-4 top-4" dialogTitle="New Task"
+            dialogDescription="Just an example.">
+        </TaskDialog>
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                <div
+                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                <div
+                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                <div
+                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
                 </div>
             </div>
