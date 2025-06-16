b119e8e278ccccbb6640ac01f2dc47b4526fe7d5
Leonardo Gil <leogilc@gmail.com>
2025-06-16 15:22:32 -0300

    bugfix: active link on sidebar

--- resources/js/components/NavMain.vue
+++ resources/js/components/NavMain.vue
@@ -3,6 +3,11 @@ import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, Sideba
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/vue3';
 
+const isActive = (href: string): boolean => {
+    const currentPath = page.url.split('?')[0];
+    return currentPath === href;
+};
+
 defineProps<{
     items: NavItem[];
 }>();
@@ -15,7 +20,7 @@ const page = usePage();
         <!-- <SidebarGroupLabel>Platform</SidebarGroupLabel> -->
         <SidebarMenu>
             <SidebarMenuItem v-for="item in items" :key="item.title">
-                <SidebarMenuButton as-child :is-active="item.href === page.url" :tooltip="item.title">
+                <SidebarMenuButton as-child :is-active="isActive(item.href)" :tooltip="item.title">
                     <Link :href="item.href">
                         <component :is="item.icon" />
                         <span>{{ item.title }}</span>
