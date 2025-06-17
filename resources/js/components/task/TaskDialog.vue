f3d03b53077a59153763333350b9a4fb47967b8f
Leonardo Gil <leogilc@gmail.com>
2025-06-16 21:33:05 -0300

    task form in a component

--- resources/js/components/task/TaskDialog.vue
+++ resources/js/components/task/TaskDialog.vue
@@ -1,18 +1,5 @@
 <script setup lang="ts">
-import { useForm } from 'vee-validate'
-import { toTypedSchema } from '@vee-validate/zod'
 import { Button } from '@/components/ui/button'
-import * as z from 'zod'
-import {
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from '@/components/ui/form'
-import { Textarea } from '@/components/ui/textarea'
-import { Input } from '@/components/ui/input'
 import {
     Dialog,
     DialogContent,
@@ -22,17 +9,10 @@ import {
     DialogTitle,
     DialogTrigger,
 } from '@/components/ui/dialog'
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select';
-import { router } from '@inertiajs/vue3'
+
 import { ref } from 'vue';
-import { toast } from 'vue-sonner'
+import TaskForm from './TaskForm.vue';
+
 
 /** Props vindas do parent */
 defineProps<{
@@ -42,34 +22,7 @@ defineProps<{
     dialogDescription: string
 }>()
 
-const formSchema = toTypedSchema(z.object({
-    title: z.string().min(2).max(50),
-    description: z.string().min(2).max(255),
-    due_date: z.string(),
-    priority: z.enum(['low', 'medium', 'high']),
-}))
-
-const { isFieldDirty, handleSubmit } = useForm({
-    validationSchema: formSchema,
-})
-
 const open = ref(false)
-
-const onSubmit = handleSubmit((values) => {
-    router.post(route('tasks.store'), {
-        ...values,
-    }, {
-        onSuccess: () => {
-            open.value = false
-            toast.success('Task ' + values.title + ' created successfully!', {
-                position: 'top-center'
-            })
-        },
-        onError: () => {
-            toast.error('Something went wrong!')
-        }
-    })
-})
 </script>
 
 <template>
@@ -86,69 +39,8 @@ const onSubmit = handleSubmit((values) => {
                 <DialogDescription>
                     {{ dialogDescription }}
                 </DialogDescription>
+                <TaskForm />
             </DialogHeader>
-
-            <form @submit="onSubmit" class="space-y-4">
-                <FormField v-slot="{ componentField }" name="title" :validate-on-blur="!isFieldDirty">
-                    <FormItem>
-                        <FormLabel>Title</FormLabel>
-                        <FormControl>
-                            <Input type="text" v-bind="componentField" />
-                        </FormControl>
-                        <FormDescription>
-                            Please enter a title.
-                        </FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-                <FormField v-slot="{ componentField }" name="description" :validate-on-blur="!isFieldDirty">
-                    <FormItem>
-                        <FormLabel>Description</FormLabel>
-                        <FormControl>
-                            <Textarea type="text" v-bind="componentField" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-                <div class="grid grid-cols-2 gap-4">
-                    <div>
-                        <FormField v-slot="{ componentField }" name="due_date" :validate-on-blur="!isFieldDirty">
-                            <FormItem>
-                                <FormLabel>Due Date</FormLabel>
-                                <FormControl>
-                                    <Input type="date" v-bind="componentField" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        </FormField>
-                    </div>
-                    <div>
-                        <FormField v-slot="{ componentField }" name="priority" :validate-on-blur="!isFieldDirty">
-                            <FormItem>
-                                <FormLabel>Priority</FormLabel>
-                                <FormControl>
-                                    <Select id="priority" v-bind="componentField">
-                                        <SelectTrigger class="w-full">
-                                            <SelectValue placeholder="Select a priority" />
-                                        </SelectTrigger>
-                                        <SelectContent>
-                                            <SelectGroup>
-                                                <SelectItem value="low">Low</SelectItem>
-                                                <SelectItem value="medium">Medium</SelectItem>
-                                                <SelectItem value="high">High</SelectItem>
-                                            </SelectGroup>
-                                        </SelectContent>
-                                    </Select>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        </FormField>
-                    </div>
-                </div>
-                <Button type="submit">
-                    Submit
-                </Button>
-            </form>
         </DialogContent>
     </Dialog>
 </template>
